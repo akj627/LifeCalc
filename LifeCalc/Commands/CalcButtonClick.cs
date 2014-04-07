e7119aeb6a5cff9bea0cbf44fd1f7e25420ab65a
akj627 <achintya.jha@gmail.com>
2014-04-07 02:52:49 -0400

    Before doing last app requirement of fast switching

---
+++ LifeCalc/Commands/CalcButtonClick.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LifeCalc.Commands
+{
+    public class CalcButtonClick : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- LifeCalc/MainPage.xaml.cs
+++ LifeCalc/MainPage.xaml.cs
@@ -22,13 +22,54 @@ namespace LifeCalc
             //BuildLocalizedApplicationBar();
         }
 
+        public void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtLoanAmt.Text = "";
+            txtIntRate.Text = "";
+            txtTaxes.Text = "";
+            txtHOA.Text = "";
+            radTerm30.IsChecked = true;
+            tblMortgage.Text = "";
+            pieChart.Series[0].DataPoints[0].Value = 0;
+            pieChart.Series[0].DataPoints[1].Value = 0;
+
+        }
+
         private void btnCalc_Click(object sender, RoutedEventArgs e)
         {
-            double loanAmt = Convert.ToDouble(txtLoanAmt.Text);                   // price of total mortgage before down payment
-            //double taxesPerYear = (double)txtPropertyTax.CurrentValue;                    // this will divided by 12 and added to the monthly payment
-            //double downPayment = (double)txtDownPayment.CurrentValue;            // down payment will be subtracted from the loan
-            double intRate = (Convert.ToDouble(txtIntRate.Text)) / 100;                               // calculate interest from 100%
-            double loanDuration = 120;                        // monthly term
+            if (string.IsNullOrEmpty(txtLoanAmt.Text.Trim()))
+            {
+                MessageBox.Show("Please enter loan amount");
+                txtLoanAmt.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtIntRate.Text.Trim()))
+            {
+                MessageBox.Show("Please enter interest rate");
+                txtIntRate.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtTaxes.Text.Trim()))
+            {
+                MessageBox.Show("Please enter annual property tax");
+                txtTaxes.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtHOA.Text.Trim()))
+                txtHOA.Text = "0";
+
+            // Loan amout after downpayment
+            double loanAmt = Convert.ToDouble(txtLoanAmt.Text);
+            // taxes per year to be divide by 12 and added to monthly payment
+            double taxesPerMonth = (Convert.ToDouble(txtTaxes.Text)) / 12;
+            double hoaPerMonth = Convert.ToDouble(txtHOA.Text);
+            // interest rate
+            double intRate = (Convert.ToDouble(txtIntRate.Text)) / 100;
+            // default term
+            double loanDuration = 360;
             if (radTerm10.IsChecked.HasValue && radTerm10.IsChecked.Value)
                 loanDuration = 120;
             if (radTerm15.IsChecked.HasValue && radTerm15.IsChecked.Value)
@@ -38,12 +79,12 @@ namespace LifeCalc
             if (radTerm30.IsChecked.HasValue && radTerm30.IsChecked.Value)
                 loanDuration = 360;
 
-            //double propertyTax = (double)txtPropertyTax.CurrentValue;
-            //double insurance = (double)txtInsurance.CurrentValue;
-
-            // plug the values from the input into the mortgage formula
+            // mortgage formula
 
             double payment = (loanAmt) * (Math.Pow((1 + intRate / 12), loanDuration) * intRate) / (12 * (Math.Pow((1 + intRate / 12), loanDuration) - 1));
+            // add taxes
+            payment = payment + taxesPerMonth;
+            payment = payment + hoaPerMonth;
             var mortgage = Math.Round(payment, 2, MidpointRounding.AwayFromZero);
             tblMortgage.Text = mortgage.ToString();
 
